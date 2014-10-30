d4839c6f66d9a9d6fbc52a303a1ed95df442e6d5
Bill Zhou <bill@micrometrics.ca>
2014-10-30 10:32:17 -0400

    Install active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!  
+  # before_action :authenticate_user!  
+  before_action :custom_authentication!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:role)
+  end
+
+  def custom_authentication!
+    open_routes = ['/s/','/hotspot/','admin/','active_admin/devise/sessions']
+    authenticate_user! unless open_routes.any? { |path| 
+      "/#{params[:controller]}/".include? path
+    }
   end
 end
